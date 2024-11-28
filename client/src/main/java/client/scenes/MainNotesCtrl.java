1982a53f8a064e71ce321e496a07b6c4fe8ba7b9
Stijn van Drimmelen <s.l.b.drimmelen@student.tudelft.nl>
2024-11-28 16:28:25 +0100

    Set up the base of the Notes Application

---
+++ client/src/main/java/client/scenes/MainNotesCtrl.java
@@ -0,0 +1,13 @@
+package client.scenes;
+
+import javafx.stage.Stage;
+
+public class MainNotesCtrl {
+    private Stage primaryStage;
+
+    public void initialize(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+
+        primaryStage.show();
+    }
+}
