ef6ab6edb4f1c4a78c3fa830d1908cb000729fdc
Vasco Dos Santos Roldao <v.m.santosroldao@student.tudelft.nl>
2025-01-20 19:23:29 +0100

    Made it so that the listView with the files updates if the note is deleted

--- client/src/main/java/client/scenes/NoteOverviewCtrl.java
+++ client/src/main/java/client/scenes/NoteOverviewCtrl.java
@@ -1018,6 +1018,7 @@ public class NoteOverviewCtrl implements Initializable, UpdateListener {
         clearFields();
         server.deleteNoteById(selectedNote.getId(), getCurrentCollection().getServer());
         server.deleteFile(selectedNote.getId(), null);
+        noteFiles.setAll(server.fetchFileNames(selectedNote.getId()));
         try {
             System.out.println("Sending delete message for note with ID: " + selectedNoteId);
             stompClient.send("SEND\n" + "destination:/app/note-deletions\n\n" + objectMapper.writeValueAsString(selectedNote) + "\0");
