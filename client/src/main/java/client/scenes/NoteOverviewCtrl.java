98f351b7c8cedcbdbd5c7a5c22cfe212714f3cfd
Iasonas Chronis <i.l.chronis@student.tudelft.nl>
2025-01-25 22:50:35 +0100

    Added collection shortcut for opening menu

--- client/src/main/java/client/scenes/NoteOverviewCtrl.java
+++ client/src/main/java/client/scenes/NoteOverviewCtrl.java
@@ -710,6 +710,12 @@ public class NoteOverviewCtrl implements Initializable, UpdateListener {
                 keyPressedDOWN(e);
                 e.consume();
                 break;
+            case RIGHT:
+                keyPressedRIGHT(e);
+                e.consume();
+            case LEFT:
+                keyPressedLEFT(e);
+                e.consume();
             case T:
                 keyPressedT(e);
                 e.consume();
@@ -764,6 +770,28 @@ public class NoteOverviewCtrl implements Initializable, UpdateListener {
                 break;
             default:
                 break;
+        }
+    }
+    /**
+     * Treats the key pressed event for the RIGHT key
+     * @param e key pressed
+     */
+    public void keyPressedRIGHT(KeyEvent e) {
+        if (e.isAltDown()) {
+            collectionMenu.show();
+            setCurrentCollection();
+            e.consume();
+        }
+    }
+    /**
+     * Treats the key pressed event for the LEFT key
+     * @param e key pressed
+     */
+    public void keyPressedLEFT(KeyEvent e) {
+        if (e.isAltDown()) {
+            collectionMenu.show();
+            setCurrentCollection();
+            e.consume();
         }
     }
 
